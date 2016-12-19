823d2f21e0cc0cff345f535f61bf6ba1219c6d7f
Saulius Čebanauskas <saulius@cebanauskai.lt>
2016-12-19 22:29:13 +0200

    Day 17

---
+++ Days/Day17.js
@@ -0,0 +1,30 @@
+﻿module.exports = function Day17() {
+    const util = require('util');
+    var crypto = require('crypto');
+    var Stopwatch = require("node-stopwatch").Stopwatch;
+    var input = 'ihgpwlah';
+    var availableLetters = ['b', 'c', 'd', 'e', 'f'];
+    //var input = 'awrkjxxr';
+    function canGo(letter) {
+        if (letter in availableLetters) {
+        }
+    }
+
+    function getAvailableDirections(path) {
+        var md5 = crypto.createHash('md5').update(path).digest("hex");
+        var availableDirections = [];
+    }
+
+    this.part1 = function () {
+        var stopwatch = Stopwatch.create();
+        stopwatch.start();
+        stopwatch.stop();
+        console.log(util.format('Day 17 part 1 answer: %s. Total duration: %s ms', 123, stopwatch.elapsedMilliseconds));
+    }
+    this.part2 = function () {
+        var stopwatch = Stopwatch.create();
+        stopwatch.start();
+        stopwatch.stop();
+        console.log(util.format('Day 17 part 2 answer: %s. Total duration: %s ms', 321, stopwatch.elapsedMilliseconds));
+    }
+}
--- app.js
+++ app.js
@@ -21,30 +21,30 @@ require('./Utils/Extensions')
 //day3.part1();
 //day3.part2();
 //-----------
-var Day4 = require('./Days/Day4');
-var day4 = new Day4();
-day4.part1();
-day4.part2();
+//var Day4 = require('./Days/Day4');
+//var day4 = new Day4();
+//day4.part1();
+//day4.part2();
 //-----------
 //var Day5 = require('./Days/Day5');
 //var day5 = new Day5();
 //day5.part1();
 //day5.part2();
 //-----------
-var Day6 = require('./Days/Day6');
-var day6 = new Day6();
-day6.part1();
-day6.part2();
+//var Day6 = require('./Days/Day6');
+//var day6 = new Day6();
+//day6.part1();
+//day6.part2();
 //-----------
 //var Day7 = require('./Days/Day7');
 //var day7 = new Day7();
 //day7.part1();
 //day7.part2();
 //-----------
-var Day8 = require('./Days/Day8');
-var day8 = new Day8();
-day8.part1();
-day8.part2();
+//var Day8 = require('./Days/Day8');
+//var day8 = new Day8();
+//day8.part1();
+//day8.part2();
 //-----------
 //var Day9 = require('./Days/Day9');
 //var day9 = new Day9();
